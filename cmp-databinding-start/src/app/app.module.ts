6183fcaf301ec802aba55b7447bc2b5b40aef4ff
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-05-17 12:38:04 -0700

    Imported cmp-databinding-start project and created 2 new modules
    The focus of this lesson is to take this app which has 1 app file
    containing all the code and break down those parts into modules so it is
    easier to work with

---
+++ cmp-databinding-start/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementtComponent } from './server-elementt/server-elementt.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CockpitComponent,
+    ServerElementtComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ cmp-databinding-start/src/app/server-elementt/server-elementt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-elementt',
+  templateUrl: './server-elementt.component.html',
+  styleUrls: ['./server-elementt.component.css']
+})
+export class ServerElementtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
