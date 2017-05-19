85372a53c807266e1c63d1b6d0044183ec3d63b3
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-05-19 10:23:38 -0700

    Added alias for blueprintCreated function and binding

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,7 @@ export class CockpitComponent implements OnInit {
 
   // New properties created for app.component
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // @Output() is the angular function we use to output the data
   // <> is used to define the type of event data we are going to emit
   // {} is what event we define, this one takes the object from /app/app.component.ts of the onServerAdded() method
