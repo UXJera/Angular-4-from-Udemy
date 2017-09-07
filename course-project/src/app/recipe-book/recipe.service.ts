fd616d6a841ec1ac67ec350dda399fbcea7a70d9
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-09-07 10:05:48 -0700

    Adding Ingredients to Recipe Model

--- course-project/src/app/recipe-book/recipe.service.ts
+++ course-project/src/app/recipe-book/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 
 @Injectable()
@@ -8,8 +9,21 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'http://lorempixel.com/output/food-q-c-640-480-5.jpg'),
-    new Recipe('Another Test Recipe', 'Again is simply a test', 'http://lorempixel.com/output/food-q-c-640-480-6.jpg'),
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome',
+      'http://lorempixel.com/output/food-q-c-640-480-5.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?', 'http://lorempixel.com/output/food-q-c-640-480-6.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]),
   ];
 
   getRecipes() {
