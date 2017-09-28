2e6fbd4c17a435c4e1951932cb298ba20d27c28e
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-09-28 12:14:43 -0700

    Adding Editing Routes

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 @NgModule({
@@ -37,6 +38,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     FooterComponent,
     DropdownDirective,
     RecipeStartComponent,
+    RecipeEditComponent,
 
   ],
   imports: [
