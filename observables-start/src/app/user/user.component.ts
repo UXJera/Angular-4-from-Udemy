70696cc52f90387c29d3dbeee8d266cf66e34c5e
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-09-29 11:58:35 -0700

    Analyzing a Built-in Angular Observable

--- observables-start/src/app/user/user.component.ts
+++ observables-start/src/app/user/user.component.ts
@@ -13,10 +13,21 @@ export class UserComponent implements OnInit {
 
   ngOnInit() {
     this.route.params
+    // params is an asyncronous object that recieves data from Angular and assigns the id according to what the id is in the http://.../[id]
       .subscribe(
+        // subscribe is our Observer
         (params: Params) => {
           this.id = +params['id'];
+          // This is our Handle Data
+        }, () => {
+          // This is our Handle Error
+        }, () => {
+          // This is our Handle Complete
         }
+        // subscribe takes 3 arguments: Handle Data, Handle Error, and Handle Complete
+        // The only Handle that makes sense is the first one
+        // We subscribing to an observable which wraps a data source (the data source being code in angular which emits a new parameter whenever we click a link)
+        // Once the click resolves, the data package is pushed and recieved by our subsriber (the 3 methods inside the .subscribe() ).
       );
   }
 
