0a167cbb50040c46251f76241bda42a965026a4f
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-09-29 13:00:44 -0700

    Unsubscribe

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {Observer} from 'rxjs/Observer';
 import 'rxjs/Rx';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubscription: Subscription;
+  customObsSubscription: Subscription;
+  // We are creating these variables to store the subscription information in, rather than having them be self-executing within the code. This makes it much easier to clean up and stop the code if we move away from this component
 
   constructor() { }
 
   ngOnInit() {
-    //const myNumbers = Observable.interval(1000);
-    // // A new piece of data will be emitted every 1000ms or 1 second
-    // myNumbers.subscribe(
-    //   (number: Number) => {
-    //     console.log(number)
-    //   }
-    //);
+    const myNumbers = Observable.interval(1000);
+    // A new piece of data will be emitted every 1000ms or 1 second
+    this.numbersObsSubscription = myNumbers.subscribe(
+      // We are saving the subscription to a variable (which has the class of subscription), meaning nothing will really change as far as output goes, but it is easier to stop the code from running infinitely
+      (number: Number) => {
+        console.log(number)
+      }
+    );
 
     // We want to create an observable that will fire after 2 seconds, 4 seconds, and will fail at 5 seconds, and also what would happen if it completes
 
@@ -44,7 +49,7 @@ export class HomeComponent implements OnInit {
         // This never gets fired off
       }
     );
-    myObservable.subscribe(
+    this.customObsSubscription = myObservable.subscribe(
       (data: string) => {console.log(data);},
       (error: string) => {console.log(error);},
       () => {console.log('Completed');}
@@ -52,6 +57,12 @@ export class HomeComponent implements OnInit {
     );
     // Create takes a function as an argument which holds our asyncronous code
 
+  };
+
+  ngOnDestroy() {
+    this.numbersObsSubscription.unsubscribe();
+    this.customObsSubscription.unsubscribe();
   }
+
 
 }
