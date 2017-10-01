e67ecc74aac6bc2357a9dd18d7326d90e77887cc
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-10-01 12:03:36 -0700

    Cleaning up Project and Adding Observable
    Replaced EventEmitter with Subject and added .unsubscribe()
    functionality to the component using the rxjs/Subject

--- course-project/src/app/shopping-list/shopping-list.service.ts
+++ course-project/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,11 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subject } from 'rxjs/Subject';
+
 @Injectable()
 export class ShoppingListService {
 
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
@@ -16,7 +18,7 @@ export class ShoppingListService {
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient)
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   constructor() { }
@@ -28,7 +30,7 @@ export class ShoppingListService {
     // The way above is alright when dealing with smaller amounts of data, but the next version is better.
     // The ... is ES6 for spread
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
 }
