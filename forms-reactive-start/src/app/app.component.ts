423ea166bb33e78e9ba2b2b239ed34a3fd3ae7b1
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-10-03 12:48:16 -0700

    Creating Custom Validators

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -9,13 +9,14 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUsernames = ['Chris', 'Anna'];
 
 
   ngOnInit() {
     // We want to do this on OnInit before the page is rendered
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [ Validators.required, this.forbiddenNames.bind(this)]),
         'email':    new FormControl(null, [Validators.required, Validators.email]),
       }),
       'gender':   new FormControl('male'),
@@ -30,5 +31,17 @@ export class AppComponent implements OnInit{
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get('hobbies')).push(control)
+  }
+
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    // The function will take in a FormControl argument and return a js object
+    // The js object will take a key [s: string] => typescript's way of saying 'Hey, we want a key:value pair', the key can be interpreted as a string
+    // {nameIsForbidden: true}
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true };
+    }
+    return null;
+    // If validation is successful, either nothing or 'null' is passed
+    // return {'nameIsForbidden': false }; would not work
   }
 }
