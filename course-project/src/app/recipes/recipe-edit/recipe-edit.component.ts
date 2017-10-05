c0c777eb010dd595fa1495caa61db815b4af2c85
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-10-05 12:13:18 -0700

    Submitting and Saving the Recipe Form

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Params} from '@angular/router';
 import {FormGroup, FormControl, FormArray, Validators} from '@angular/forms';
 
 import {RecipeService} from '../recipe.service';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -30,7 +31,19 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.recipeForm);
+    // const newRecipe = new Recipe(
+    //   this.recipeForm.value['name'],
+    //   this.recipeForm.value['description'],
+    //   this.recipeForm.value['imagePath'],
+    //   this.recipeForm.value['ingredients']);
+
+    if (this.editMode) {
+      //this.recipeService.updateRecipe(this.id, newRecipe);
+      // Since our form is set up to be matching fields, we can simply just add the value
+      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+    } else {
+      this.recipeService.addRecipe(this.recipeForm.value);
+    }
   }
 
   onAddIngredient() {
