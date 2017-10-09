85bf4763a0cab4402e07922dc07de67e765fc4f7
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-10-09 14:07:11 -0700

    Pure and Impure Pipes

--- pipes-start/src/app/filter.pipe.ts
+++ pipes-start/src/app/filter.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false // true by default
 })
 export class FilterPipe implements PipeTransform {
 
