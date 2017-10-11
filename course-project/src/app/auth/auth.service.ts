8b0119b5fa45b56d709114effa95ed62e3d8fb01
Jeremiah Nyman <jeremiahnyman@gmail.com>
2017-10-11 11:53:27 -0700

    Sending th Auth Token Back to Server

--- course-project/src/app/auth/auth.service.ts
+++ course-project/src/app/auth/auth.service.ts
@@ -5,6 +5,8 @@ import * as firebase from 'firebase';
 @Injectable()
 export class AuthService {
 
+  token: string;
+
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password).catch(
       error => console.log(error)
@@ -13,10 +15,21 @@ export class AuthService {
 
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password).then(
-      response => console.log(response)
+      response => {
+        firebase.auth().currentUser.getIdToken().then(
+          (token: string) => this.token = token
+        )
+      }
     ).catch(
       error => console.log(error)
     );
+  }
+
+  getToken() {
+    firebase.auth().currentUser.getIdToken().then(
+      (token: string) => this.token = token
+    ); // This method only returns a Promise since there is server interaction
+    return this.token;
   }
 
   constructor() { }
--- course-project/src/app/shared/data-storage.service.ts
+++ course-project/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 
 import {RecipeService} from '../recipes/recipe.service';
+import {AuthService} from '../auth/auth.service';
 
 import {Recipe} from '../recipes/recipe.model';
 
@@ -11,7 +12,8 @@ import 'rxjs/Rx';
 export class DataStorageService {
   constructor(
     private http: Http,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private authService: AuthService
   ) {}
 
   dbServer = "https://ng-recipe-book-9d32a.firebaseio.com/recipes.json";
@@ -19,13 +21,17 @@ export class DataStorageService {
 
 
   storeRecipes() {
-    return this.http.put(this.dbServer, this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put(this.dbServer + '?auth=' + token, this.recipeService.getRecipes());
     // getRecipes will get a copy of the recipes and store them in an array
     // We want to use PUT because we want to overwrite the old data
   }
 
   getRecipes() {
-    return this.http.get(this.dbServer)
+    const token = this.authService.getToken();
+
+    this.http.get(this.dbServer + '?auth=' + token)
     .map(
       (response: Response) => {
         const recipes: Recipe[] = response.json();
@@ -43,4 +49,6 @@ export class DataStorageService {
       }
     );
   }
+
+
 }
